04ea526cec071f5c5cb9f8b8eff0bd9e76320402
azkahrtmi <azkahartami10@gmail.com>
2025-06-02 09:55:44 +0700

    feat: some update

--- TaskManagement.Infrastructure/obj/Debug/net8.0/TaskManagement.Infrastructure.AssemblyInfo.cs
+++ TaskManagement.Infrastructure/obj/Debug/net8.0/TaskManagement.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+206637721c474532b417673f6546d16e109a5fad")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
